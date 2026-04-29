24adf6f9052c0fa16df9a1317d8118a9b4c3deec
zzy <2218847957@qq.com>
2026-04-29 17:18:03 +0800

    Initial Commit

---
+++ Tirego/TiregoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiregoApp.swift
+//  Tirego
+//
+//  Created by yangyang on 2026/4/29.
+//
+
+import SwiftUI
+
+@main
+struct TiregoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
